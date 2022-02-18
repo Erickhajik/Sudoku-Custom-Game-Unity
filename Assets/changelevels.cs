8c612c505efa874efe5affe83e77145af0536952
Erickhajik <erickhajik65@gmail.com>
2022-02-17 17:21:02 -0800

    First Upload

---
+++ Assets/changelevels.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changelevels : MonoBehaviour
+{
+    public void playrandom()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+   
+}
