3d3794f552ac1f7c7317b0f318a627283bd0a8f9
Erickhajik <erickhajik65@gmail.com>
2022-03-06 19:07:10 -0800

    Update changelevels.cs

--- Assets/changelevels.cs
+++ Assets/changelevels.cs
@@ -5,13 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class changelevels : MonoBehaviour
 {
-    public void level1()
-    {
-        SceneManager.LoadScene("1");
-    }
+    // public Animation anim1;
+    //
+    // void Start()
+    // {
+    //     anim1 = gameObject.GetComponent<Animation>();
+    // }
+    // void Update()
+    // {
+    //     anim1.
+    //     if (!anim1.isPlaying)
+    //     {
+    //         SceneManager.LoadScene("1");
+    //     }
+    //     
+    // }
+    public GameObject level;
 
+    public void levelDisable()
+    {
+        level.SetActive(false);
+    }
     public void MainMenu()
     {
-        SceneManager.LoadScene("SudokuGame");
+        
+        SceneManager.LoadSceneAsync("1");
     }   
+    public void LoadMainMenu()
+        {
+            
+            SceneManager.LoadSceneAsync("SudokuGame");
+        }   
 }
